c31b53340aade115a5b0d8d0bce3d95059b521e6
Perederko Ruslan <peruongithub@gmail.com>
2017-06-06 17:56:06 +0300

    add ReadOnlyProperty class

---
+++ src/ReadOnlyProperty.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * This file is part of the Trident package.
+ *
+ * (c) Perederko Ruslan <perederko.ruslan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Trident\Components\Property;
+
+use Trident\Components\Property\Interfaces\PropertyInterface;
+use Trident\Components\Property\Traits\PropertyTrait;
+
+class ReadOnlyProperty implements PropertyInterface
+{
+    use PropertyTrait;
+
+    public function __construct(string $name, $value = null)
+    {
+        $this->name = $name;
+        $this->setValue($value);
+    }
+
+    public function setValue($value = null):PropertyInterface
+    {
+        /** @var $this PropertyInterface */
+        return $this;
+    }
+
+    public function setDefault($default = null):PropertyInterface
+    {
+        /** @var $this PropertyInterface */
+        return $this;
+    }
+}
